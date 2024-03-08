6272f6bac7260a26d4f90a88a80a97eb36952ba4
Shahana S <shahanasha2001@gmail.com>
2024-03-09 00:15:34 +0530

    Update HomePage.jsx

--- src/Customer/pages/HomePage/HomePage.jsx
+++ src/Customer/pages/HomePage/HomePage.jsx
@@ -7,14 +7,12 @@ const HomePage = () => {
   return (
     <div>
         <MainCarousel/>
-    
-        <div className=' space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+         <div className=' space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Kurta"}/>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shoes"}/>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shirt"}/>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Women's Saree"}/>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Women's dress"}/>
-            
         </div>
     </div>
   )
