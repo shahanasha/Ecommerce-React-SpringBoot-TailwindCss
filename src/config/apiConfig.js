48c7882ba98c35ca0f490bd583857cc622c037ed
Shahana <shahanasha2001@gmail.com>
2024-03-16 16:31:49 +0530

    product details updateion

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-export const API_BASE_URL="http://localhost:8080"
+export const API_BASE_URL= "https://ecommerce-server-production-54ba.up.railway.app"
+
 const jwt=localStorage.getItem("jwt");
 
 export const api=axios.create({
